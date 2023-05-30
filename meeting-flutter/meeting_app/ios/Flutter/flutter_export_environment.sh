8bc793cea7a1d8852e5af932c31d9e106148af62
zhouxiaolu <zhouxiaolu@corp.netease.com>
2023-05-30 14:20:36 +0800

    fix: 优化来电逻辑

---
+++ meeting-flutter/meeting_app/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/zhouxiaolu/Flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/zhouxiaolu/Netease/Github/NetEase_Meeting/meeting-flutter/meeting_app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=3.10.0"
+export "FLUTTER_BUILD_NUMBER=31000"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
